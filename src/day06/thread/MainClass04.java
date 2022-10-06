e79207d73f4387fb73b67f735db048c5a0a6becc
ITSC <ITSC@DESKTOP-ONE4R81>
2022-10-06 19:50:00 +0900

    10/06 수정

--- src/day06/thread/MainClass04.java
+++ src/day06/thread/MainClass04.java
@@ -1,24 +1,27 @@
 package day06.thread;
 
-import day06.thread.mypac.CountTread;
+import day06.thread.mypac.Brother;
+import day06.thread.mypac.Sister;
+import day06.thread.mypac.Wallet;
 
 public class MainClass04 {
-	
 	public static void main(String[] args) {
-		CountTread cThread = new CountTread();
+		// Wallet 객체 생성하기
+		Wallet wallet = new Wallet();
 		
-		cThread.start();
+		// Sister 스레드 객체 생성
+		Sister sister = new Sister(wallet);
 		
-		for (int i = 0; i < 10; i++) {
-			System.out.println(i*100);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-			}
-			
-			
-		}
-		System.out.println("메인 스레드가 종료됩니다.");
+		// Brother 스레드 객체 생성
+		Brother brother = new Brother(wallet);
+		
+		// 각각의 객체를 이용해서 두개의 스레드 시작시키기
+		sister.start();
+		brother.start();
+		
+		System.out.println("매인스레드가 종료됩니다.");
+		
+		
 	}
-	
+
 }
